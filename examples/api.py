44777e2c56b3bafd3d8019234f5e060cf72e39bd
Vivek Shrivastava <platinum012@gmail.com>
2019-03-22 23:23:58 +0530

    Added requests demo
    - Added sample code to demonstrate API calls using requests and json modules
    - Different types of url parameters samples added
    - Has link to github repository by digital ocean of the public API URLs.

---
+++ examples/api.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 22-03-2019 at 10:02 PM
+
+@author: Vivek
+
+NOTE: For the purpose of demo, I am using various public APIs from the Github repository: https://github.com/davemachado/public-api, powered by Digital Ocean
+      The service was still in beta at the time of my usage, and if you are using the code at a later time, there may be some issues.
+      The demo is only for explaining the usage of requests module must only be used for learning the basics. In a proper project, other additional measures might be needed to make sure
+      that the APIs are used/deployed properly and safely.
+"""
+
+import requests
+import json
+
+URL_BASE = 'https://api.publicapis.org/'
+URL_GET_CATEGORY = 'entries'
+URL_GET_RANDOM = 'random'
+URL_GET_ALL_CATEGORIES = 'categories'
+
+class API:
+    def __init__(self):
+        print('Setting up API...')
+
+        # Control parameters
+        self.enableAPICall = True
+
+        # API parameters
+        self.apiURL = None
+        self.apiHeader = None
+        self.apiPayload = None
+        self.response = None
+
+
+    def getEntry(self,categoryName):
+        print('\nFetching category:',categoryName)
+        self.apiURL = URL_BASE + URL_GET_CATEGORY
+        self.apiPayload = {'category':categoryName,'https':True}
+
+        if self.enableAPICall is True:
+            self.response = requests.get(url=self.apiURL,params=self.apiPayload)
+            responseJSON = json.loads(self.response.text)
+            for key, value in responseJSON.items():
+                print(' -', key, '=', value)
+
+
+    def getRandom(self):
+        print('\nFetching random category...')
+        self.apiURL = URL_BASE + URL_GET_RANDOM
+        self.apiPayload = {'auth':'null'}
+
+        print('URL:',self.apiURL)
+        print('Query params:',self.apiPayload)
+
+        if self.enableAPICall is True:
+            self.response = requests.get(url=self.apiURL,params=self.apiPayload)
+            responseJSON = json.loads(self.response.text)
+            for key, value in responseJSON.items():
+                print(' -', key, '=', value)
+
+
+    def getAllCategories(self):
+        print('\nFetching list of all categories...')
+        self.apiURL = URL_BASE + URL_GET_ALL_CATEGORIES
+        print(self.apiURL)
+
+        if self.enableAPICall is True:
+            self.response = requests.get(url=self.apiURL)
+            responseList = json.loads(self.response.content)
+            return responseList
+        else:
+            return list()
+
+if __name__ == '__main__':
+    obj = API()                             # Create class object
+    obj.getRandom()                         # Calling get API to fetch an entry in a random category
+
+    # Getting and displaying the list of all categories
+    categories = obj.getAllCategories()
+    print(categories)
+
+    # Getting a specific category entry
+    obj.getEntry('business')
